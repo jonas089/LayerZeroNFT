3f11a0ea4f4ce4f5a2eb3a07d36182e85b6a424d
jonas089 <jonaspauli089@gmail.com>
2022-08-24 17:57:55 +0200

    add site

--- NFTBackend/main.py
+++ NFTBackend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 #########################
 #   BACKEND NFT         #
@@ -29,5 +29,8 @@ def uri():
     )
     return jsonify(metadata)
 
+@app.route('/demo')
+def demo():
+    return render_template('index.html')
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
